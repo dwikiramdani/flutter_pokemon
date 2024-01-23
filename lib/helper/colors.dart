f034f49fac76c3317f08cd549b8c5bd3453b3638
BATTLE-STATION\dwiki <official.dwikiramdani@gmail.com>
2024-01-24 00:27:00 +0700

    Define Colors scheme for each of pokemon elements

---
+++ lib/helper/colors.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class PokeTypeColor{
+  static const Color fairy = Color(0xFFE69EAC);
+  static const Color ice = Color(0xFF9AD6DF);
+  static const Color grass = Color(0xFF74CB48);
+  static const Color steal = Color(0xFFB7B9D0);
+  static const Color rock = Color(0xFFB69E31);
+  static const Color dark = Color(0xFF75574C);
+  static const Color physic = Color(0xFFFB5584);
+  static const Color water = Color(0xFF6493EB);
+  static const Color ghost = Color(0xFF70559B);
+  static const Color ground = Color(0xFFDEC16B);
+  static const Color dragon = Color(0xFF7037FF);
+  static const Color electric = Color(0xFFF9CF30);
+  static const Color fire = Color(0xFFF57D31);
+  static const Color bug = Color(0xFFA7B723);
+  static const Color poison = Color(0xFFA43E9E);
+  static const Color flying = Color(0xFFA891EC);
+  static const Color fighting = Color(0xFFC12239);
+  static const Color normal = Color(0xFFAAA67F);
+}
